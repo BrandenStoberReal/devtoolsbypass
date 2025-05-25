904c9a6a1c0e70ae7bae5f1c21c6f82c85898740
Branden Stober <67252380+BrandenStoberReal@users.noreply.github.com>
2025-05-24 22:05:26 -0400

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @author       set8
 // @version      1.4
-// @description  Enhanced DevTools-detection bypass (console, hooks, timing, visibility, eval, etc). Tends to break more advanced sites.
+// @description  Enhanced DevTools-detection bypass (console, hooks, timing, visibility, eval, etc). Tends to break more advanced sites (specifically cloudflare challenges).
 // @match        *://*/*
 // @grant        none
 // @run-at       document-start
